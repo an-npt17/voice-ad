c3cd1f2628717e149c43878bbab289116753b05f
an-npt17 <truongan1412@gmail.com>
2025-08-30 21:30:53 +0700

    use denoise

--- update_model.py
+++ update_model.py
@@ -1,7 +1,9 @@
 import torch
+from omegaconf import OmegaConf
 
-model, utils = torch.hub.load(
-    repo_or_dir="snakers4/silero-vad", model="silero_vad", force_reload=True, onnx=True
+torch.hub.download_url_to_file(
+    "https://raw.githubusercontent.com/snakers4/silero-models/master/models.yml",
+    "latest_silero_models.yml",
+    progress=False,
 )
-
-(get_speech_timestamps, save_audio, read_audio, VADIterator, collect_chunks) = utils
+models = OmegaConf.load("latest_silero_models.yml")
